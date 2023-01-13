b414289778badedb436be1b68e19172a5c5c6be0
noahmolloy <122574487+noahmolloy@users.noreply.github.com>
2023-01-12 16:19:13 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+	x = "ECE_180_DA_DB"
+	if x == "EE_180_DA_DB":
+		print("You are living in 2017")
+	else:
+		# like comment and subscribe
+		x = x + " - Best class ever"
+		print(x)
